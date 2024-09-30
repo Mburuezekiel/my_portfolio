09e87fe034cbee632556580525a7236be68af4a7
Mburu Ezekiel <125687134+Mburuezekiel@users.noreply.github.com>
2024-09-30 08:40:10 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
           <Route path="/" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-        // <Projects />
+        
         <Footer />
       </div>
     </Router>
